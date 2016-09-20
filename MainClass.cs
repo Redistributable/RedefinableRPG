b800130ade3daeee9526679238f4b3fce4c63ff6
Redistributable <a32real@yahoo.co.jp>
2016-09-20 23:56:47 +0900

    公開
    hello, world!!

---
+++ MainClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Redefinable.Applications.RapidRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class MainClass
+    {
+        /// <summary>
+        /// アプリケーションのメインエントリポイント
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
